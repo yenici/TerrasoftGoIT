f76015857e4df2f1a099eb8b14a41f4850118f3e
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-07-13 17:22:13 +0300

    Assignment 1.2 is done.

---
+++ Assignment_01_2/Assignment_01_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assignment_01_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello world";
+            foreach(char c in hello)
+            {
+                Console.WriteLine("Char\t{0}:\tdec {1,5},\thex {2,4:x}", c, Convert.ToUInt32(c), Convert.ToUInt32(c));
+            }
+        }
+    }
+}
