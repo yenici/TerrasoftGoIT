03fc3220a3dcb75de8d55fb27c1a967e04788987
Dmytro Novikov <Dmytro.G.Novikov@gmail.com>
2016-08-08 00:04:29 +0300

    Old commented lines of code deleted from  Programm.cs

--- Assignment_07_01/Program.cs
+++ Assignment_07_01/Program.cs
@@ -10,31 +10,9 @@ namespace Assignment_07_01
     {
         static void Main(string[] args)
         {
-            //Employee[] employees = new Employee[] { new Doctor(), new Psychologist(), new Security(), new Worker() };
-            //foreach(var employee in employees)
-            //{
-            //    employee.CalculateSalary();
-            //}
-            //Console.WriteLine("=======================================================");
-            //ISalary[] payment = new ISalary[] { new Doctor(), new Psychologist(), new Security(), new Worker(),
-            //    new Trainee(new Doctor()), new Trainee(new Security())};
-            //foreach (var employee in payment)
-            //{
-            //    employee.CalculateSalary();
-            //}
             EmployeesList employees = new EmployeesList();
             employees.CreateFooList();
             employees.StartMenu();
         }
     }
-    enum Employees
-    {
-        Employee,
-        Trainee,
-        Doctor,
-        Psychologist,
-        Guard,
-        Worker
-    }
-
 }
